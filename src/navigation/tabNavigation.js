32c0a2b950f4d5435be08834758494745e04f5b1
Eugene Varnashin <nevadaru@yandex.ru>
2022-08-23 18:37:33 +0300

    add tab icons

--- src/navigation/tabNavigation.js
+++ src/navigation/tabNavigation.js
@@ -4,7 +4,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import HomePage from '../screen/HomePage';
 import FavoritePage from '../screen/FavoritePage';
 import ComparePage from '../screen/ComparePage';
-import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,11 +12,32 @@ const TabNavigation = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        screenOptions={{
+        screenOptions={({route}) => ({
           headerShown: false,
           tabBarInactiveTintColor: 'gray',
           tabBarActiveTintColor: '#DDBEC3',
-        }}>
+
+          tabBarIcon: ({color, size}) => {
+            let iconName;
+
+            if (route.name === 'HomePage') {
+              iconName = 'pokeball';
+            }
+            if (route.name === 'FavoritePage') {
+              iconName = 'cards-heart-outline';
+            }
+            if (route.name === 'ComparePage') {
+              iconName = 'compare';
+            }
+            return (
+              <MaterialCommunityIcons
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            );
+          },
+        })}>
         <Tab.Screen name={'FavoritePage'} component={FavoritePage} />
         <Tab.Screen name={'HomePage'} component={HomePage} />
         <Tab.Screen name={'ComparePage'} component={ComparePage} />
